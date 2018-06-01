3a3ca1e28e78d9afa4dbe77202ebbaf14ca86cf3
macstudent <macstudent@10.10.150.192>
2018-06-01 16:30:27 -0400

    added details

---
+++ src/employeemanagementsystem/Arithematic.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagementsystem;
+
+/**
+ *
+ * @author macstudent
+ */
+public class Arithematic {
+     
+    public int sum(int a,int b)
+    {
+        int c=a+b;
+        return c;
+    }
+}
